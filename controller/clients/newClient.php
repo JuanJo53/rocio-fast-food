ad79a7edbf680b0215d4b5b414df4c37298384c0
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-10-05 02:08:49 -0400

    add new client fast method in sales page done

--- controller/clients/newClient.php
+++ controller/clients/newClient.php
@@ -4,21 +4,29 @@
     $name=$_POST['client_name'];
     $nit=$_POST['client_nit'];
     
+    $origin=false;
+
     if(isset($_POST['client_phone'])){
-        $phone=$_POST['client_phone'];        
+        $phone=$_POST['client_phone'];
+        $origin=true;
     }else{
+        $origin=false;
         $phone=0;
     }
     if(isset($_POST['client_email'])){        
         $email=$_POST['client_email'];
+        $origin=true;
     }else{
         $email="";
+        $origin=false;
     }
 
     $client = new Client;
     $result = $client->newClient($name,$nit,$phone,$email);
-    
+
+    if($origin==false){
+        header('Location: ../../view/sales.php');
+    }
     // echo $result;
-    // header('Location: ../../view/clients.php');
 
 ?>
