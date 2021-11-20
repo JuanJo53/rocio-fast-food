a7c922b19adbdcddcea61f44b37107df45e544ee
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-11-19 22:44:26 -0400

    new product with img fixed

--- controller/products/newProduct.php
+++ controller/products/newProduct.php
@@ -9,15 +9,15 @@
     $stock=$_POST['prod_stock'];
     
     $product = new Product;
-    if(!empty($_FILES["imageE"]["name"])) { 
+    if(!empty($_FILES["image"]["name"])) { 
         // Get file info 
-        $fileName = basename($_FILES["imageE"]["name"]); 
+        $fileName = basename($_FILES["image"]["name"]); 
         $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
          
         // Allow certain file formats 
         $allowTypes = array('jpg','png','jpeg','gif');
         if(in_array($fileType, $allowTypes)){ 
-            $image = $_FILES['imageE']['tmp_name']; 
+            $image = $_FILES['image']['tmp_name']; 
             $imgContent = addslashes(file_get_contents($image));
             $result = $product->newProduct($name,$desc,$idCat,$idProv,$price,$stock,$imgContent);
         }else{
